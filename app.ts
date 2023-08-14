ab0deaab23c744886bf485bcd7a57cd191b47beb
Paulo <paulocorreapc93@gmail.com>
2023-08-13 22:57:14 -0400

    tipos de arrays

--- app.ts
+++ app.ts
@@ -25,3 +25,29 @@ let stringOrNumber: string | number;
 stringOrNumber = 12
 
 stringOrNumber = "paulo"
+
+// ARRAY
+
+let names = ["paulo", "gabriel", "barros"]
+
+names.push("correa")
+
+// names.push(2) ERRO
+
+let numbers = [1, 2, 3]
+
+numbers.push(4)
+
+// numbers.push("paulo") ERRO
+
+//  ARRAYS COM TIPOS
+
+let testStringArray: string[]
+
+// testStringArray = [1, 2, 3] ERRO
+
+testStringArray = ["paulo", "correa"]
+
+let testNumberArray: number[]
+
+testNumberArray = [1, 2, 2]
