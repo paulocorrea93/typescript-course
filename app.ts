e9992b8c260da066a20a4aed96901e7055c1ec26
Paulo <paulocorreapc93@gmail.com>
2023-08-13 22:50:56 -0400

    tipos de variáveis simples

---
+++ app.ts
@@ -0,0 +1,17 @@
+let variable = "hello";
+
+variable = "hi"
+
+let age = 30;
+
+let ageWithType: number
+
+ageWithType = 30;
+
+let testString: string;
+
+testString = "hello"
+
+let testBoolean: boolean;
+
+testBoolean = true;
