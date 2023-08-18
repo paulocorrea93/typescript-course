1969f3d6a1c21d0af52342c7c2a110e5bc59dbd0
Paulo <paulocorreapc93@gmail.com>
2023-08-18 09:30:46 -0400

    validação com if no typescript

--- app.ts
+++ app.ts
@@ -1,30 +1,39 @@
 // FUNÇÕES ANONIMAS TYPESCRIPT
 
-setTimeout (() => {
-    const sallary: number = 1000
+setTimeout(() => {
+  const sallary: number = 1000;
 
-    // console.log(parseFloat(sallary))
-    console.log(sallary)
-
-}, 2000)
+  // console.log(parseFloat(sallary))
+  console.log(sallary);
+}, 2000);
 
 // TIPOS DE OBJETOS
 
-function passCoordinades(coord: {x: number, y: number}){
-    console.log(`X coordinades: ${coord.x}`)
-    console.log(`Y coordinades: ${coord.y}`)
+function passCoordinades(coord: { x: number; y: number }) {
+  console.log(`X coordinades: ${coord.x}`);
+  console.log(`Y coordinades: ${coord.y}`);
 }
 
-const objCoord = {x: 384, y: 948}
+const objCoord = { x: 384, y: 948 };
 
-passCoordinades(objCoord)
+passCoordinades(objCoord);
 
 //PROPRIEDADES OPICIONAIS
 
 let showNumbers = (a: number, b: number, c?: number) => {
-    console.log(`"A" = ${a}`)
-    console.log(`"B" = ${b}`)
-    console.log(`"C" = ${c}`)
-} 
+  console.log(`"A" = ${a}`);
+  console.log(`"B" = ${b}`);
+  console.log(`"C" = ${c}`);
+};
 
-showNumbers(1, 2)
+showNumbers(1, 2);
+
+let advancedGreetings = (firstName: string, lastName?: string) => {
+  if (lastName !== undefined) {
+    return `Olá, ${firstName} ${lastName}, tudo bem?`;
+  }
+  return `Olá ${firstName}, tudo bem?`;
+};
+
+console.log(advancedGreetings("Paulo", "Correa"));
+console.log(advancedGreetings("Gabriel"));
