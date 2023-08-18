b7a5db8264097dc9294f1da47fc15052459d0ba3
Paulo <paulocorreapc93@gmail.com>
2023-08-18 09:07:46 -0400

    propriedades opcionais

---
+++ app.ts
@@ -0,0 +1,30 @@
+// FUNÇÕES ANONIMAS TYPESCRIPT
+
+setTimeout (() => {
+    const sallary: number = 1000
+
+    // console.log(parseFloat(sallary))
+    console.log(sallary)
+
+}, 2000)
+
+// TIPOS DE OBJETOS
+
+function passCoordinades(coord: {x: number, y: number}){
+    console.log(`X coordinades: ${coord.x}`)
+    console.log(`Y coordinades: ${coord.y}`)
+}
+
+const objCoord = {x: 384, y: 948}
+
+passCoordinades(objCoord)
+
+//PROPRIEDADES OPICIONAIS
+
+let showNumbers = (a: number, b: number, c?: number) => {
+    console.log(`"A" = ${a}`)
+    console.log(`"B" = ${b}`)
+    console.log(`"C" = ${c}`)
+} 
+
+showNumbers(1, 2)
