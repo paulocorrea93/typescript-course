f07425de470eafb8f474053b4bb3b7b4e84492d5
Paulo <paulocorreapc93@gmail.com>
2023-08-20 13:38:40 -0400

    type guard

--- NARROWING/app.js
+++ NARROWING/app.js
@@ -1,1 +1,16 @@
 "use strict";
+// TYPE GUARD
+function sum(a, b) {
+    if (typeof a === "string" && typeof b === "string") {
+        console.log(parseFloat(a) + parseFloat(b));
+    }
+    else if (typeof a === "number" && typeof b === "number") {
+        console.log(a + b);
+    }
+    else {
+        console.log("Impossível realizar operação.");
+    }
+}
+sum("10", "20");
+sum(20, 20);
+sum("5", 3);
---
+++ NARROWING/app.ts
@@ -0,0 +1,15 @@
+// TYPE GUARD
+
+function sum(a: number | string, b: number | string){
+    if(typeof a === "string" && typeof b === "string"){
+        console.log(parseFloat(a) + parseFloat(b))
+    } else if (typeof a === "number" && typeof b === "number"){
+        console.log(a + b)
+    } else {
+        console.log("Impossível realizar operação.")
+    }
+}
+
+sum("10", "20")
+sum(20, 20)
+sum("5", 3)
