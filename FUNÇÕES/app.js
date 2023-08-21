ba0caa8a62212bcdc64180c56079afc615beb61b
Paulo <paulocorreapc93@gmail.com>
2023-08-21 10:16:13 -0400

    funções como callback

--- FUNÇÕES/app.js
+++ FUNÇÕES/app.js
@@ -4,3 +4,14 @@ const withoutReturn = () => {
     console.log("Essa é uma função sem retorno");
 };
 withoutReturn();
+// FUNÇÕES COMO CALLBACK
+const greeting = (name) => {
+    return `Olá ${name}`;
+};
+const preGreeting = (f, userName) => {
+    console.log("Preparando a função");
+    const greet = f(userName);
+    console.log(greet);
+};
+preGreeting(greeting, "Paulo");
+preGreeting(greeting, "Gabriel");
--- FUNÇÕES/app.ts
+++ FUNÇÕES/app.ts
@@ -5,3 +5,19 @@ const withoutReturn = (): void => {
 };
 
 withoutReturn();
+
+
+// FUNÇÕES COMO CALLBACK
+
+const greeting = (name: string) => {
+    return `Olá ${name}`
+}
+
+const preGreeting = (f: (name: string) => string, userName: string) => {
+    console.log("Preparando a função")
+    const greet = f(userName)
+    console.log(greet)
+}
+
+preGreeting(greeting, "Paulo")
+preGreeting(greeting, "Gabriel")
