3de1328eff5e18e6e8c87183eee6000b76b7b7e0
Paulo <paulocorreapc93@gmail.com>
2023-08-23 14:03:23 -0400

    generics

---
+++ CRIACAOTIPOS/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1 de 9 - GENERICS
+const showData = (arg) => {
+    return `o dado é: ${arg}`;
+};
+console.log(showData(10));
+console.log(showData("Dez"));
+console.log(showData(false));
+console.log(showData([1, 2, 3]));
