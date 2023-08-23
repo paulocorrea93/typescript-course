dde06b29bd8ec40f2ca744e5b016ac2628b6e853
Paulo <paulocorreapc93@gmail.com>
2023-08-23 14:11:26 -0400

    constraint em generics

--- CRIACAOTIPOS/app.js
+++ CRIACAOTIPOS/app.js
@@ -7,3 +7,10 @@ console.log(showData(10));
 console.log(showData("Dez"));
 console.log(showData(false));
 console.log(showData([1, 2, 3]));
+// 2 de 9 constraint em generics
+const showProductName = (obj) => {
+    return `o nome do produto é: ${obj.name}`;
+};
+console.log(showProductName({ name: "roupa", size: "gg" }));
+console.log(showProductName({ name: "porta", color: "branca" }));
+// console.log(showProductName({nOfShirts: 50})) APRESENTA ERRO, POIS O OBJETO PRECISA TER A PROPRIEDADE 'NAME'
--- CRIACAOTIPOS/app.ts
+++ CRIACAOTIPOS/app.ts
@@ -8,3 +8,13 @@ console.log(showData(10))
 console.log(showData("Dez"))
 console.log(showData(false))
 console.log(showData([1, 2, 3]))
+
+// 2 de 9 constraint em generics
+
+const showProductName = <T extends {name: string}> (obj: T) => {
+    return `o nome do produto é: ${obj.name}`
+}
+
+console.log(showProductName({name: "roupa", size: "gg"}))
+console.log(showProductName({name: "porta", color: "branca"}))
+// console.log(showProductName({nOfShirts: 50})) APRESENTA ERRO, POIS O OBJETO PRECISA TER A PROPRIEDADE 'NAME'
