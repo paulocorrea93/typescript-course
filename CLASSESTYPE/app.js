f8de679df29e9b04849fd3dd6df8886be3ccfa91
Paulo <paulocorreapc93@gmail.com>
2023-08-24 10:08:20 -0400

    constructor

--- CLASSESTYPE/app.js
+++ CLASSESTYPE/app.js
@@ -6,3 +6,13 @@ class User {
 const Paulo = new User;
 Paulo.name = "Paulo";
 console.log(Paulo);
+// constructor
+class NewUser {
+    constructor(name, age) {
+        this.age = age;
+        this.name = name;
+    }
+}
+const joao = new NewUser("João", 30);
+console.log(joao);
+// const pedro = new NewUser(20, 30) **APRESENTA ERRO, POIS OS PARAMETROS PASSADOS DEVEM RESPEITAR O TIPO**
