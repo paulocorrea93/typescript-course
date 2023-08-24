88f0a370ea703233697c407c1de6465d63d87ac0
Paulo <paulocorreapc93@gmail.com>
2023-08-24 10:01:11 -0400

    campos das classes

---
+++ CLASSESTYPE/app.js
@@ -0,0 +1,8 @@
+"use strict";
+//92 a 113
+// campos de classes
+class User {
+}
+const Paulo = new User;
+Paulo.name = "Paulo";
+console.log(Paulo);
---
+++ CLASSESTYPE/app.ts
@@ -0,0 +1,14 @@
+//92 a 113
+
+// campos de classes
+
+class User {
+  name!: string;
+  age!: number;
+}
+
+const Paulo = new User
+
+Paulo.name = "Paulo"
+
+console.log(Paulo)
